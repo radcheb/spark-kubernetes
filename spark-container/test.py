6ba34399e61346839cb4d3a32140e600f8d5ccfd
Radhwane Chebaane <radcheb@gmail.com>
2020-03-25 16:34:37 +0100

    test script

--- spark-container/test.py
+++ spark-container/test.py
@@ -4,5 +4,8 @@ spark_builder = SparkSession.builder.master("local[*]")
 
 spark = spark_builder.getOrCreate()
 
-uri = "s3://predictive-services-data-staging/expirable/dpam/purchase_vectorized/de105ac0-835f-4ce3-be2b-86bd14c5a1c9/data/"
+uri = "s3://path/to/data/"
 
+df = spark.read.parquet(uri)
+
+df
